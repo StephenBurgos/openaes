cdf6d6f0273f937fb8996885e1e82e9f6a51b14c
Stephen Burgos <stephen.burgos782004@gmail.com>
2015-04-07 15:03:35 -0400

    timing.c compiles with g++ now

--- timing.c
+++ timing.c
@@ -1,5 +1,11 @@
-#include <ctime>
+#include <time.h>
 #include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned long long timestamp_t;
+const int MICROSECONDS_PER_SECOND = 1000000.0L;
 
 timestamp_t get_timestamp()
 {
@@ -17,10 +23,16 @@ void calc_timing(timestamp_t time0, timestamp_t time1)
 	double secs = (time1 - time0) / (MICROSECONDS_PER_SECOND * 1.0);
 	long usecs = time1 - time0;
 	//std::cout << "Time taken (secs): " << secs << ", usecs: " << usecs << std::endl;
-	printf("Time taken: %f seconds, %f microseconds\n");
+	printf("Time taken: %f seconds, %ld microseconds\n", secs, usecs);
 }
 
-timestamp_t time0 = get_timestamp();
-// Code here
-timestamp_t time1 = get_timestamp();
-calc_timing(time0, time1); // Final Time
+int main()
+{
+
+	timestamp_t time0 = get_timestamp();
+	// Code here
+	timestamp_t time1 = get_timestamp();
+	calc_timing(time0, time1); // Final Time
+
+	return 0;
+}
